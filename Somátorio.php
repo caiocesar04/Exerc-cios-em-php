6eda235f21f25d342046271dd00836cfd865afcf
caiocesar04 <55161837+caiocesar04@users.noreply.github.com>
2022-01-30 08:35:57 -0300

    Create Somátorio.php

---
+++ Somátorio.php
@@ -0,0 +1,8 @@
+
+<?php
+ $somatorio = 0;
+  for($i = 1; $i <= 100; $i++){
+ $somatorio += $i;
+    }
+     echo "somatório dos números de 1 a 100: " . $somatorio ; echo ", Média da soma:" . $somatorio/100;
+?>
