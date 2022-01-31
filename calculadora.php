6d2818af0025c492f93d134e3d21500f0d9df376
caiocesar04 <55161837+caiocesar04@users.noreply.github.com>
2022-01-31 18:15:46 -0300

    Update calculadora.php

--- calculadora.php
+++ calculadora.php
@@ -1,31 +1,37 @@
+<!DOCTYPE html>
+<body>
+
+
 <?php
-$resposta_calculo=null
-
+$resposta_calculo=null;
 for ($i=0;$i<4;$i++)
-$sel[]=null;
-
+	$sel[]=null;
 if(isset($_GET['num1']) && isset($_GET['num2']))
 {
-	switch ($_GET['num'])
+switch ($_GET['num'])
 	{
-	  //operações de multiplicação(adição,multiplicação)//
-	case '+':
-		$resposta_calculo = $_GET['num1'] +  $_GET['num2']; $sel[0]= 'selected';
-		break;
-    case '*':
-		$resposta_calculo = $_GET['num1'] *  $_GET['num2']; $sel[1]= 'selected';
-		break;
-		//operações de subtração(subtração e divisão)//
-		case '-':
-		$resposta_calculo = $_GET['num1'] -  $_GET['num2']; $sel[2]= 'selected';
-		break;
-	case '/':
-		$resposta_calculo = $_GET['num1'] /  $_GET['num2']; $sel[3]= 'selected';
-		break;
+	case '+': $resposta_calculo = $_GET['num1'] +  $_GET['num2'];$sel[0]= 'selected'; break;
+	case '-': $resposta_calculo = $_GET['num1'] -  $_GET['num2']; $sel[1]= 'selected'; break;
+	case '*': $resposta_calculo = $_GET['num1'] *  $_GET['num2']; $sel[2]= 'selected'; break;
+	case '/': $resposta_calculo = $_GET['num1'] /  $_GET['num2']; $sel[3]= 'selected'; break;
 	}	
-	else{
-	$_GET['num1']='0'; $_GET['num']='+'; $_GET['num2']='0';
-}
-
 }
 ?>
+<form>
+    Informe dois números e escolha a operação que deseja fazer
+    <br>
+    (Soma,Subtração,Multipliação,Divisão). 
+    <br>
+    Depois, clique no botão com sinal e igual para exibir o resultado
+    <br>
+    <br>
+	<input  type = number name = "num1" value = "<?=$_GET['num1'] ?>"/>
+	<select name = "num">
+        //soma// <option <?= $sel[0]?>>+</option> //subtração// <option <?= $sel[1]?>>-</option> //multoplicação// <option <?= $sel[2]?>>*</option> //divisão // <option <?= $sel[3]?>>/</option>
+	</select>
+	<input type = number name = "num2" value = "<?=$_GET['num2']?>"/>
+	<button>=</button>
+	<?=$resposta_calculo?>
+</form>
+</body>
+</html>
