b8c705df9fe2774930bd364192a8b11b1f4671b1
caiocesar04 <55161837+caiocesar04@users.noreply.github.com>
2022-03-04 15:31:20 -0300

    Create Menu.php

---
+++ Menu.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html> 
+<html>
+<head>
+<meta charset="utf-8">
+<title>Sistema Vídeo Locadora IFPR</title>
+</head>
+<body>
+<h1> VÍDEO LOCADORA IFPR </h1>    
+</body>
+<?php
+include("conexao.php");
+?>
+<p><img src="filme.jpg" width="319" height="289" alt=""/></p>
+    
+</body>
+</head>
+</html>
